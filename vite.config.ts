8fbf1726159183c67e3b57e33f7ece26b6676836
Renwarren <warren.chendjou@gmail.com>
2026-01-31 21:05:05 -0500

    Fix vite config to add cloudflare tryme host

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    allowedHosts: [".trycloudflare.com", "localhost", "127.0.0.1", "::1"],
+  },
 });
